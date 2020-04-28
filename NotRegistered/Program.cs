ad224ecd99530b996e5050ea7fadc2589fa6bcd5
Vasilis Gaitanidis <vasilis.gaitanidis@yahoo.gr>
2020-04-28 12:34:10 +0300

    Add project files.

---
+++ NotRegistered/Program.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using System;
+using Autofac.Features.ResolveAnything;
+
+namespace NotRegistered
+{
+    class Program
+    {
+        interface ISpeakable
+        {
+            void Speak();
+        }
+
+        class Person : ISpeakable
+        {
+            public void Speak()
+            {
+                Console.WriteLine("Hello");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+
+            // on-the-fly registration source
+            builder.RegisterSource(new AnyConcreteTypeNotAlreadyRegisteredSource());
+
+            using var container = builder.Build();
+
+            var person = container.Resolve<Person>();
+            person.Speak();
+        }
+    }
+}
